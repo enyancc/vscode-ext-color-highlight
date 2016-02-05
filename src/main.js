7786b0b6edcab14760f9484d691ce059969d3950
Sergii Naumov <sergii.naumov@1und1.de>
2016-02-05 15:24:36 +0100

    Rename enabled option

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ let context;
 function activate (ctx) {
   config = vscode.workspace.getConfiguration('color-highlight');
 
-  if (!config.enabled) {
+  if (!config.enable) {
     config = null;
     return false;
   }
