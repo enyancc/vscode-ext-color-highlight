61b859eaf50dba558f01ee0d9d23171ec5e31346
Sergii Naumov <sergii.naumov@1und1.de>
2017-03-27 17:01:54 +0200

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,32 @@ module.exports = {
   entry: './src/main.js',
   output: {
     libraryTarget: 'commonjs2',
-    filename: 'dist/extension.js'
+    filename: 'dist/extension.js',
   },
   externals: {
     'vscode': 'vscode'
   },
-  module: {}
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['latest', {
+              es2015: {
+                modules: false
+              }
+            }]],
+            plugins: [
+              'transform-runtime'
+            ]
+          }
+        }
+      }
+    ]
+  }
 };
 
 if (process.env.NODE_ENVIRONMENT !== 'production') {
