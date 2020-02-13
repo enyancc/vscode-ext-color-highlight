eed15a92fa65d8f3b9d616c295d4466d2537a2fd
TessavWalstijn <twalstijn@hotmail.com>
2020-02-13 15:45:40 +0100

    migrated vscode to vscode-test && added 2 tests

--- test/extension.test.js
+++ test/extension.test.js
@@ -10,7 +10,7 @@ var assert = require('assert');
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-var vscode = require('vscode');
+var vscode = require('vscode-test');
 var myExtension = require('../extension');
 
 // Defines a Mocha test suite to group tests of similar kind together
