0dedac2395e00d4f0928b07346e5c7afc095f8b9
Tim Maffett <timmaffett@gmail.com>
2022-03-03 13:10:43 -0800

    Fix findHex() to correctly handle hex color codes which include alpha digits (either 4 digit or 8 digit hex codes)- and IGNORE them.

--- src/strategies/hex.js
+++ src/strategies/hex.js
@@ -18,7 +18,17 @@ export async function findHex(text) {
   while (match !== null) {
     const firstChar = match[0][0];
     const matchedColor = match[1];
-    const matchedHex = '#' + match[2];
+    // Here we check for possibility of hex codes with ALPHA (0xaabbcc vs 0xffaabbcc) 
+    // or (0xF0FF vs 0x0FF)  
+    // and remove the the alpha part (the first digit in 4 digit hex or first 2 digits
+    // in an 8 digit hex)
+    // Otherwise the color we capture gets interpreted incorrectly by Color() below.
+    //   The two hex codes above SHOULD BE the same color if the corrected version of the extension is running.
+    const matchedHex = '#' + 
+                      (match[2].length==4 ? 
+                          (match[2].substring(1, 4)) : 
+                          ( (match[2].length==8) ? match[2].substring(2, 8) : match[2])
+                      );
     const start = match.index + (match[0].length - matchedColor.length);
     const end = colorHex.lastIndex;
 
